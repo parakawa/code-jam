3bace3fbb6e48b3fdff18694b8e4c0f03761467c
parakawa <paty.arakawa@gmail.com>
2016-04-14 15:40:28 -0500

    intento 2

--- codys-jams2.rb
+++ codys-jams2.rb
@@ -1,5 +1,21 @@
 #Code Jam to I/O 2016 for Women
 #Problema A: Cody's Jams
+def cody(x)
+	y=[]
+	w=["solucion"]
+	i=1
+	while x!=nil do
+	y[0]=x[0].to_i
+	
+	if y[0] =x[i].to_i*0.75
+		w.push(x[0].to_i)
+		x.delete_at(0)
+		x.delete_at(i-1)
+		i=1
+	else i=i+1
+	end
+end
+print w
 
 puts "Ingrese números separados por un espacio"
 x=gets.chomp.split(" ")
